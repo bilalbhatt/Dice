a993e84cc2684a6a5be8f16da85b165192f02785
Bilal Bhat <70620874+bilalbhatt@users.noreply.github.com>
2022-12-11 19:12:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNum1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage = "dice" + randomNum1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNum2 = Math.floor(Math.random()*6)+1;
+var randomImageSource2 = "images/dice" + randomNum2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNum1 > randomNum2){
+    document.querySelector("h1").innerHTML = "player 1 won";
+
+}
+else if (randomNum2 > randomNum1){
+    document.querySelector("h1").innerHTML = "player 2 won";
+
+}
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
